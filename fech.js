3a8d89513c112fbda1b244ddfb0a6063bbdf5cec
Your Name <you@example.com>
2026-03-25 15:53:28 +0700

    update files

--- fech.js
+++ fech.js
@@ -2,10 +2,9 @@ let demo = document.getElementById('demo');
 fetch('http://localhost:3000/products')
     .then(res => res.json())
     .then(data =>{
-        // console.log(data);
         data.forEach(item => {
             demo.innerHTML +=`
                 <img src="image/${item.image}" alt"" width="300">  
-            `;
+            `;                          
         })
     }) 
